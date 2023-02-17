a2fd6efd07ccc1a37204309ade94200e98bda2d0
SmileJun <godhkekf24@inha.edu>
2023-02-17 15:47:26 +0900

    백준 17609번 회문 문제풀이

--- src/Baekjoon/String/Q_17609.java
+++ src/Baekjoon/String/Q_17609.java
@@ -1,18 +1,49 @@
 package Baekjoon.String;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
-/**
- * 조커 하나 들고 있는데 하나씩 확인하다가 다르면 조커 카드 확인, 없으면 아웃
- * 있으면 조커 -- 하고 idx1을 하나 늘리거나 idx2를 하나 줄여서 다시 확인
- * front 인덱스가 back 인덱스를 같거나 넘어가는 순간 완료
- * 완료 시 조커 카드가 그대로 있으면 0 없으면 1
- */
 public class Q_17609 {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int result;
+    public static void main(String[] args) throws IOException {
+        int T = Integer.parseInt(br.readLine());
+        for(int tc = 1; tc <= T; tc++) {
+            String str = br.readLine();
+            result = 2;
+            check(str, 0, str.length() - 1, 1);
+            bw.write(result + "\n");
+        }
+        bw.close();
+    }
+
+    private static void check(String str, int start, int end, int joker) {
+        //끝까지 왔다
+        if(start >= end) {
+            if(joker == 1) {
+                result = 0;
+                return;
+            }
+
+            result = 1;
+            return;
+        }
+
+        //만약 같으면 다음 단계
+        if(str.charAt(start) == str.charAt(end)) {
+            check(str, start + 1, end - 1, joker);
+            return;
+        }
+
+        //다르면 조커 있는지 확인
+        if(joker == 0) {
+            return;
+        }
+
+        //조커 있으면
+        check(str, start + 1, end, 0);
+        check(str, start, end - 1, 0);
     }
 
 }
